611a04fb66ae2033902150a84884c1222b156330
Daniel Engvall <daniel@engvalls.eu>
2019-03-06 22:18:28 +0100

    Some changes

--- image_displayer.py
+++ image_displayer.py
@@ -1,11 +1,20 @@
 import Tkinter as tkinter
 from PIL import Image
 import sys
+from io import BytesIO
+import StringIO
+from base64 import b64decode
 
 
-def convert_png_to_gif(input_png):
-    im = Image.open(input_png)
-    im.save(input_png.replace('.png', '.gif'))
+def get_image_data(input_file=None, base64_data=None, input_format='png'):
+    if input_file:
+        io_in = BytesIO(open(input_file, 'rb').read())
+    elif base64_data:
+        io_in = StringIO(b64decode(base64_data))
+    image = Image.open(io_in)
+    filename = 'tmp.gif'
+    image.save(filename)
+    return filename
 
 
 def quit_ui(*args):
@@ -18,8 +27,8 @@ def callback():
 
 
 def run():
-    IMAGE ='./image.png'
-    convert_png_to_gif(IMAGE)
+    IMAGE ='./image.gif'
+    image_data = get_image_data(input_file=IMAGE)
     root = tkinter.Tk()
     root.configure(background="black")
 
@@ -27,8 +36,9 @@ def run():
     canvas.pack(fill=tkinter.BOTH, expand=tkinter.YES)
     canvas.configure(background='black')
 
-    img_file = tkinter.PhotoImage(file=IMAGE.replace('.png', '.gif'))
-    img = canvas.create_image(0,0, anchor="nw", image=img_file)
+    img_file = tkinter.PhotoImage(file=image_data)
+    image_canvas = canvas.create_image(0, 0, anchor="nw", image=img_file)
+
 
     root.overrideredirect(True)
     root.overrideredirect(False)
